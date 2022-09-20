561db8e7bf87b070a2d769301501e29f3ef5f6c4
Margaret Sun <hi.margaret.sun@gmail.com>
2022-09-20 13:29:22 -0700

    finish  up to revString

--- recursion.js
+++ recursion.js
@@ -4,7 +4,7 @@ function product(nums) {
   if (nums.length === 0) {
     return 1;
   }
-  
+
   return nums[0] * product(nums.slice(1));
 }
 
@@ -17,11 +17,11 @@ function longest(words) {
   //hello [hola]
   //hola []
   //return ""
-  
+
   if (words.length === 0) {
     return 0
   }
-  
+
   return words[0].length > longest(words.slice(1))
           ? words[0].length
           : longest(words.slice(1));
@@ -31,31 +31,43 @@ function longest(words) {
 
 function everyOther(str) {
   if (str.length === 0) return "";
-  
+
   return str[0] + everyOther(str.slice(2));
 }
 
 /** find: return boolean depending on if val exists in array or not. */
 
 function find(arr, val) {
+  if (arr.length === 0) return false;
+
+  return arr[0] === val ? true : find(arr.slice(1), val);
 
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
+  if (str.length === 0) return true;
+
+  return str[0] === str.slice(-1) ? isPalindrome(str.slice(1,-1)) : false
 
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str) {
+  if (str.length === 0) return "";
+
+  return revString(str.slice(1)) + str[0];
 
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr, val) {
+  if (arr.length === 0) return -1;
+
+  return arr.at(-1) === val ? arr.length - 1 : findIndex(arr.splice(0, arr.length-1), val);
 
 }
 
